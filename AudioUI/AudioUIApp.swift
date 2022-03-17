e91c157ef7c16e5cc6ff51dc876b0702fc06490f
CodeNour <ayman.jibrael@icloud.com>
2022-03-17 15:41:08 +0300

    Initial Commit

---
+++ AudioUI/AudioUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioUIApp.swift
+//  AudioUI
+//
+//  Created by ayhan on 17.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AudioUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
